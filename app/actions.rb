5cec71aa86b99ffda10538e297648c4bd8617948
camronnad <camronnaderi57@gmail.com>
2022-09-25 22:57:28 +0000

    isolate html post content

--- app/actions.rb
+++ app/actions.rb
@@ -1,4 +1,19 @@
 get '/' do
-    File.read(File.join('app/views', 'index.html'))
+  username = "sharky_j"
+  avatar_url = "https://live.staticflickr.com/65535/52358606250_01c667c5da_w.jpg"
+  photo_url = "https://live.staticflickr.com/65535/52358421508_786aa10e2c_c.jpg"
+  time_ago_in_minutes = 15
+  like_count = 0
+  comment_count = 1
+  comments = [
+    "sharky_j: Out for the long weekend... too embarrassed to show y'all the beach bod!"
+  ]
+
+  if time_ago_in_minutes > 60
+    "more than an hour"
+  elsif time_ago_in_minutes== 60
+  "an hour"
+  else
+    "less than an hour"
   end
-  
+
